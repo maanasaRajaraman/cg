413d8057965888e938cb9362545cccdadffa2ba1
Maanasa Rajaraman <119788848+maanasaRajaraman@users.noreply.github.com>
2024-04-13 11:27:52 +0530

    Update cuboid.cpp

--- cuboid.cpp
+++ cuboid.cpp
@@ -103,7 +103,7 @@ void resize(int width, int height)
 
 void display(void)
 {
-    glClearColor(0.0f, 0.0f, 0.0, 0.0f);
+    glClearColor(1.0f, 1.0f, 1.0, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     mydraw();
     //drawObject();
